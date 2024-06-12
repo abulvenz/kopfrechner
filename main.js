d3a4256f50764bc148bc32d9c52a39e99fb1c5e3
Andreas Eismann <a.eismann@senbax.de>
2024-06-12 07:33:39 +0200

    feat: Creating first working version.

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 import m from "mithril";
 import tagl from "tagl-mithril";
 
-const { div, h1, p, button, input, table, tr,td,br } = tagl(m);
+const {select,option, div, h1, p, button, input, table, tr, td, br } = tagl(m);
 const { random, floor, trunc, round, ceil, abs, min, max, pow, sqrt } = Math;
+const { keys ,values} = Object;
 const use = (v, f) => f(v);
 const muse = (v, f) => f(...v);
 
@@ -11,35 +12,72 @@ const randomInt = (min, max) => floor(random() * (max - min + 1)) + min;
 const range = (n) => Array.from({ length: n }, (_, i) => i);
 
 const generators = {
-  "*": () =>
+  "×": () =>
     muse([randomInt(0, 10), randomInt(0, 10)], (a, b) => ({
-      op: "*",
+      op: "×",
       a,
       b,
       r: a * b,
+      f: (i, j) => i * j,
+    })),
+  "+": () =>
+    muse([randomInt(0, 10), randomInt(0, 10)], (a, b) => ({
+      op: "+",
+      a,
+      b,
+      r: a + b,
+      f: (i, j) => i + j,
     })),
 };
 
 const tasks = [];
+let operator = "×";
 
 m.mount(document.body, {
   view: (vnode) => [
     tasks.length === 0
-      ? button({ onclick: () => range(5).forEach( tasks.push(generators["*"]())) }, "Start")
-      : use(tasks[0], ({ op, a, b }) => [a, op, b, "=", br(),
-        table(
-            range(11).map(i=>tr(
-                range(11).map(j=>td(button.solution[i===j?"middle":""]({onclick:()=>{
-                    if (tasks.r === tasks.f(i,j)){
-                        tasks.shift();
-                    }
-                    else {
-                        tasks.push(tasks.shift());
-                    }
-                },style:{cursor:"pointer"}},`${i * (j)}`)))
-            ))
-        )
-
-      ]),
+      ? [
+          select({              onchange: (e) =>console.log(e.target.value)|| (operator = e.target.value),
+          },
+          keys(generators).map((op) =>
+            option({
+              value: op,
+            },op)
+          )),
+          button(
+            {
+              onclick: () =>
+                range(5).forEach((e) => tasks.push(generators[operator]())),
+            },
+            "Start"
+          ),
+        ]
+      : use(tasks[0], ({ op, a, b, r, f }) => [
+          div.task(a, " ", op, " ", b, " = "),
+          table(
+            range(100).map((i) =>
+              tr(
+                range(10).map((j) =>
+                  td(
+                    button.solution[i === j ? "middle" : ""](
+                      {
+                        onclick: () => {
+                          if (r === i * 10 + j) {
+                            tasks.shift();
+                          } else {
+                            tasks.push(tasks.shift());
+                          }
+                        },
+                        style: { cursor: "pointer" },
+                      },
+                      `${i * 10 + j}`
+                    )
+                  )
+                )
+              )
+            )
+          ),
+          div.task(tasks.length, " Aufgaben"),
+        ]),
   ],
 });
